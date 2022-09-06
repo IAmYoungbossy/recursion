f6fb5c3eb68de67c113543e6c625cb68e40281f5
Letam Bossman Barinua <letambarinua@gmail.com>
2022-09-06 18:24:20 +0100

    Seperate left and right merge

--- megeSort.js
+++ megeSort.js
@@ -1,6 +1,4 @@
-/**
- * Merge sort function
- */
+// Merge sort function
 function mergeSort(array) {
 	let leftArray;
 
@@ -11,16 +9,16 @@ function mergeSort(array) {
 	if (array.length % 2 === 0) leftArray = array.splice(0, array.length / 2);
 	else leftArray = array.splice(0, Math.ceil(array.length / 2));
 
-	/* Programme alls itself until it splits each divided unit into
-  single item array then it starts returning each sorted array recursively 
-  to merge function until all sub arrays are sorted into one then programme ends.
+	/* Programme calls each of the divided halves until a single item array then
+   it starts returning each sorted array recursively to merge function until 
+   all sub arrays are sorted into one then programme ends.
   */
-	return merge(mergeSort(leftArray), mergeSort(array));
+	const left = mergeSort(leftArray),
+		right = mergeSort(array);
+	return merge(left, right);
 }
 
-/**
- * Merge function takes in 2 sorted arrays and merge into one sorted array.
- */
+// Merge function takes in 2 sorted arrays and merge into one sorted array.
 function merge(leftArray, rightArray) {
 	// Array where sorted items will be pushed to.
 	let mergedArray = [];
